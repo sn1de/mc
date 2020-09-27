20dc94639811eb3e95c9cdf6df5ffa5e709b3b41
Brad Schneider <sn1de@users.noreply.github.com>
2020-09-27 16:39:56 -0500

    Added validators and associated tests for the order model.

--- app/models/order.rb
+++ app/models/order.rb
@@ -14,4 +14,20 @@
 class Order < ApplicationRecord
   # TODO: Implement me! (part 1)
   belongs_to :product
+
+  validates :card_number, length: { is: 16 }
+  validates :card_number, numericality: { only_integer: true }
+  validates :card_exp_month, numericality: { 
+  	only_integer: true,
+  	greater_than_or_equal_to: 1,
+  	less_than_or_equal_to: 12
+  }
+  validates :card_exp_year, numericality: { only_integer: true }
+  validates :card_cvc, length: { is: 3 }
+  validates :card_cvc, numericality: { only_integer: true }
+  validates :quantity, numericality: { 
+  	only_integer: true,
+  	greater_than_or_equal_to: 1
+  }
+  validates :product_id, numericality: { only_integer: true }
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -14,5 +14,131 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # TODO: many of these could probably be replaced by simpler versions 
+  # by using shoulda-matcher
+  
+  it "is not valid without a 16 digit credit card number" do
+  	order = Order.new
+    order.card_number = "1111222233334444"
+    order.valid?
+    expect(order.errors[:card_number]).to be_empty
+
+    order.card_number = ""
+    order.valid?
+    expect(order.errors[:card_number]).to_not be_empty
+
+    order.card_number = "1"
+    order.valid?
+    expect(order.errors[:card_number]).to_not be_empty
+
+    order.card_number = "11112222333344449"
+    order.valid?
+    expect(order.errors[:card_number]).to_not be_empty
+
+    order.card_number = "1111yyyy333344449"
+    order.valid?
+    expect(order.errors[:card_number]).to_not be_empty
+  end
+
+  it "is not valid without a legitimate expiration month" do
+    order = Order.new
+    order.card_exp_month = "1"
+    order.valid?
+    expect(order.errors[:card_exp_month]).to be_empty
+
+    order.card_exp_month = "12"
+    order.valid?
+    expect(order.errors[:card_exp_month]).to be_empty
+
+    order.card_exp_month = "99"
+    order.valid?
+    expect(order.errors[:card_exp_month]).to_not be_empty
+
+    order.card_exp_month = "y"
+    order.valid?
+    expect(order.errors[:card_exp_month]).to_not be_empty
+  end
+
+  it "is not valid without a legitimate expiration year" do
+    order = Order.new
+    order.card_exp_year = "20"
+    order.valid?
+    expect(order.errors[:card_exp_year]).to be_empty
+
+    order.card_exp_year = "20y"
+    order.valid?
+    expect(order.errors[:card_exp_year]).to_not be_empty
+
+    order.card_exp_year = ""
+    order.valid?
+    expect(order.errors[:card_exp_year]).to_not be_empty
+  end
+
+  it "is not valid without a 3 digit CVC" do
+    order = Order.new
+    order.card_cvc = "123"
+    order.valid?
+    expect(order.errors[:card_cvc]).to be_empty
+
+    order.card_cvc = "1"
+    order.valid?
+    expect(order.errors[:card_cvc]).to_not be_empty
+
+    order.card_cvc = "1y3"
+    order.valid?
+    expect(order.errors[:card_cvc]).to_not be_empty
+
+    order.card_cvc = "1234"
+    order.valid?
+    expect(order.errors[:card_cvc]).to_not be_empty
+
+    order.card_cvc = "12"
+    order.valid?
+    expect(order.errors[:card_cvc]).to_not be_empty
+  end
+
+  it "is not valid without a quantity" do
+    order = Order.new
+    order.valid?
+    expect(order.errors[:quantity]).to_not be_empty
+ 
+    order.quantity = 1
+    order.valid?
+    expect(order.errors[:quantity]).to be_empty
+
+    order.quantity = 0
+    order.valid?
+    expect(order.errors[:quantity]).to_not be_empty
+ 
+    order.quantity = -2
+    order.valid?
+    expect(order.errors[:quantity]).to_not be_empty
+
+    order.quantity = 1.5
+    order.valid?
+    expect(order.errors[:quantity]).to_not be_empty
+
+    order.quantity = "five"
+    order.valid?
+    expect(order.errors[:quantity]).to_not be_empty
+  end
+
+  it "is not valid without a product_id" do
+    order = Order.new
+    order.valid?
+    expect(order.errors[:product_id]).to_not be_empty
+
+    order.product_id = 1
+    order.valid?
+    expect(order.errors[:product_id]).to be_empty
+
+    order.product_id = 1.5
+    order.valid?
+    expect(order.errors[:product_id]).to_not be_empty
+
+    order.product_id = "y"
+    order.valid?
+    expect(order.errors[:product_id]).to_not be_empty
+  end
 end
